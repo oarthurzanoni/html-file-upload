97f75323b9a72e019b33c7cf914380d9d982fb49
Mitacho <arthurf@tuta.io>
2022-02-13 10:23:58 -0300

    style: update max width and icon color

--- js/pages/index.js
+++ js/pages/index.js
@@ -1,5 +1,3 @@
-import TashIcon from "../components/trashIcon.js";
-
 import { getFileExtention, humanFileSize } from "../utils/file.js";
 import { genRandomId } from "../utils/id.js";
 
@@ -51,7 +49,17 @@ function updateFileList(files) {
           <button onclick="removeFileFromList('${
             file.id
           }')" class="icon-button">
-            ${TashIcon}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 0 24 24"
+              width="24px"
+              class="icon"
+              fill="currentColor"
+            >
+              <path d="M0 0h24v24H0z" fill="none" />
+              <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z" />
+            </svg>
           </button>
         </div>
       </div>
